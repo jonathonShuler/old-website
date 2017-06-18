0953347f0480319ae4138a3e276b7d0c10b3b2ab
Jonathon Shuler <jonathon.shuler@gmail.com>
2017-06-18 17:19:41 -0600

    Add site content, minor js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 
+var status = "jS";
+
+$(window).scroll(function(){
+	
+	if($(this).scrollTop()>= 300 && status !== "jonathonShuler") {
+		$("#initials-name").text("jonathonShuler");
+		status = $("#initials-name").text();
+	}
+	
+	if($(this).scrollTop()< 300 && status !== "jS") {
+		$("#initials-name").text("jS");
+		status = $("#initials-name").text();
+	}
+})
+
