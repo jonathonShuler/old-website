46b5370bfb0e545f361fe74477aff49a0bd4a90f
Jonathon Shuler <jonathon.shuler@gmail.com>
2017-07-12 19:46:35 -0600

    Update

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 
+
+//jQuery to change the text on the left side of the navbar upon scrolling to a certain point
 var status = "jS";
 
-$(window).scroll(function(){
+$(window).scroll(
+	function(){
 	
-	if($(this).scrollTop()>= 300 && status !== "jonathonShuler") {
-		$("#initials-name").text("jonathonShuler");
-		status = $("#initials-name").text();
-	}
-	
-	if($(this).scrollTop()< 300 && status !== "jS") {
-		$("#initials-name").text("jS");
-		status = $("#initials-name").text();
-	}
-})
+		if($(this).scrollTop()>= 300 && status !== "jonathonShuler") {
+			$("#initials-name").text("jonathonShuler");
+			status = $("#initials-name").text();
+		}
+		
+		if($(this).scrollTop()< 300 && status !== "jS") {
+			$("#initials-name").text("jS");
+			status = $("#initials-name").text();
+		}
+});
+
+
 
